da22e945f08a190124bc55fcddf998a3743795dd
Jake <jakeroggenbuck2@gmail.com>
2024-05-03 12:35:35 -0700

    Add exercise 1 initial

--- Src/playground/branch_main.c
+++ Src/playground/branch_main.c
@@ -6,11 +6,26 @@
 
 void branch_main() {
 	// space for initializations
+	printer_init();
 
+	buttons_init();
+
+	bool light_status = true;
 
 	// infinite loop
 	while (1) {
+		if (getSysTime() % 1000 == 0) {
+			light_status = !light_status;
+			led_a(light_status);
+		}
 
+		if (get_buttonStatus_1()) {
+			printMsg("Button 1 pressed!\r\n");
+		}
+
+		if (get_buttonStatus_2()) {
+			printMsg("Button 2 pressed!\r\n");
+		}
 	}
 
 	// NOTE
