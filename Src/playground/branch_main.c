0f15de743f3807877b20d57a36e7971a0d3cd5b4
Jake <jakeroggenbuck2@gmail.com>
2024-05-07 17:32:33 -0700

    Add changes to get it working

--- Src/playground/branch_main.c
+++ Src/playground/branch_main.c
@@ -4,30 +4,52 @@
 #include <Buttons/buttons.h>
 #include <print_scan.h>
 
+#define __NOP()                             __ASM volatile ("nop")
+
 void branch_main() {
 	// space for initializations
 	printer_init();
 
 	buttons_init();
 
+	int led_pin = 0;
+
 	bool light_status = true;
 
 	// infinite loop
 	while (1) {
-		if (getSysTime() % 1000 == 0) {
-			light_status = !light_status;
-			led_a(light_status);
+		//if (getSysTime() % 10000 < 20) {
+
+		light_status = !light_status;
+		led_pin++;
+
+		if (led_pin > 7) {
+			led_pin = 0;
+			led_dx(7, 0);
 		}
 
-		if (get_buttonStatus_1()) {
+		for (int i = 0; i < 10000; i++) {
+			nop(100);
+		}
+		//}
+
+		led_a(light_status);
+
+		led_b(!light_status);
+
+		led_dx(led_pin - 1, 0);
+		led_dx(led_pin, 1);
+
+		/*
+		if (get_buttonStatus_0()) {
 			printMsg("Button 1 pressed!\r\n");
 		}
 
-		if (get_buttonStatus_2()) {
+		if (get_buttonStatus_1()) {
 			printMsg("Button 2 pressed!\r\n");
 		}
+		*/
 	}
-
 	// NOTE
 	// Always keep the system in the infinite while loop. Never break out of it
 	// There is no place for the computer to go once you break out of this loop
